2c6f9367af4bc254eba671a450cdddfecef5ea32
David Bolin <david@bolin.at>
2019-04-03 08:35:07 -0700

    created server

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('project starting');
+'use strict';
+
+const express = require('express');
+const movies = require('./movies.json');
+const app = express();
+
+app.listen(8000, () => {
+  // eslint-disable-next-line no-console
+  console.log('server started on PORT 8000');
+});
